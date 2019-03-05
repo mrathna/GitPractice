78b99d557f587b6e2e97347aa3c2dd1e93d8f1fe
Rathna <mrathna.qa@gmail.com>
2019-03-05 13:01:34 +0530

    sync test

--- GitPractPro/Folder 1/Class1.cs
+++ GitPractPro/Folder 1/Class1.cs
@@ -20,6 +20,7 @@ namespace GitPractPro
             //added trhible comment
             // added  4 ht comment
             //fifth
+            // sync
             ChromeOptions options = new ChromeOptions();
             ChromeDriverService chrService = ChromeDriverService.CreateDefaultService("C://Drivers//");
              objDriver = new ChromeDriver(chrService, options);
